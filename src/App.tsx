d9a46ba1b5c6c1619be163761d35a57d144f5ae9
Thabang Gideon Magaola <gideon877@live.com>
2024-12-05 14:50:24 +0200

    app main files

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Dashboard from './components/Dashboard';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Login from './components/auth/Login';
+import SignUp from './components/auth/SignUp';
+import NotFound from './components/NotFound';
+import { useAuthStore } from './store/useAuthStore';
+
+const App = () => {
+	const { userId, authenticated } = useAuthStore();
+
+	console.log({userId, authenticated});
+
+	return (
+		<Router>
+			<Routes>
+				<Route path="/login" element={!authenticated ? <Login /> : <Navigate to="/" />} />
+				<Route path="/signup" element={!authenticated ? <SignUp /> : <Navigate to="/" />} />
+				<Route path="/" element={authenticated ? <Dashboard /> : <Navigate to="/login" />} />
+				{/* <Route path="/profile" element={authenticated ? <Profile /> : <Navigate to="/login" />} /> */}
+				<Route path="*" element={<NotFound />} />
+
+			</Routes>
+		</Router>
+	)
+}
+
+export default App
